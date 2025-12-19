8b195e51132330bb60bd35b4b87bdac0ee3730a0
BITS-Rohit <rohitguptaradheradhe123@gmail.com>
2025-12-19 11:56:00 +0530

    Update package name to tweakio-SDK

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="tweakio-whatsapp",
+    name="tweakio-SDK",
     version="0.1.0",
-    description="A powerful WhatsApp automation SDK using Playwright and Camoufox",
+    description="A powerful Multi-Platform automation SDK using Playwright and Camoufox. Currently supporting for whatsapp",
     author="Rohit",
     author_email="",  # Update with actual email if known, else leave blank or placeholder
     packages=find_packages(),
